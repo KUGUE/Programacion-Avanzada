0b11ef3aed913226b666e059ecd7353d16fd825a
KUGUE <63180278+KUGUE@users.noreply.github.com>
2022-09-15 18:43:59 -0600

    Add files via upload

---
+++ Unidad4 Less/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/main.css">
+    <title>PHP</title>
+</head>
+<body>
+    <div class ="container">
+            <fieldset>
+                    <form action="">
+                <legend>
+                    Dar acceso
+                </legend>
+                <label>
+                    Correo
+                </label>
+                <input type="text" name="" placehorder="write here">
+                <label>
+                    Contraseña
+                </label>
+                <input type="password" name="" placehorder="*****">
+                <button type="submit">
+                    A C C E D E R
+                </button>
+                </form>
+            </fieldset>
+  
+    </div>
+</body>
+</html>
