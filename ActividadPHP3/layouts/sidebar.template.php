621ec6864b62915cd7f026e681122777767c35d8
KUGUE <63180278+KUGUE@users.noreply.github.com>
2022-10-03 02:44:24 -0600

    Implementa el créate de producto sin imagen

---
+++ ActividadPHP3/layouts/sidebar.template.php
@@ -0,0 +1,17 @@
+
+
+<div class="col-lg-2 d-none d-sm-block">
+                <aside class="bg-gray">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Products</a>
+                        </li>
+                    </ul>
+                </aside>
+            </div>
+
+
+
