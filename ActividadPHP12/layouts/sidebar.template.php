cf38904a2f59f0eacc577abe242910c1d985fd3d
KUGUE <63180278+KUGUE@users.noreply.github.com>
2022-10-10 05:15:40 -0600

    implementar el .htaccess y BASE_PATH para ocultar la tecnología y poner una ruta absoluta en productos
    Se agrego el base path a productos.php y se arreglo el editar (no obtenia bien los elementos de los productos)

---
+++ ActividadPHP12/layouts/sidebar.template.php
@@ -0,0 +1,9 @@
+<div class="col-sm-1 d-sm-block d-none text-bg-dark sidebar">
+    <ul class="navbar-nav justify-content-end flex-grow-1 pe-2">
+        <li class="nav-item">
+        <li><a class="nav-link" href="#">Home</a></li>
+        <li><a class="nav-link" href="../product/index.php">Productos</a></li>
+        <li><a class="nav-link" href=../index.php>Salir</a></li>
+        </li>
+    </ul>
+</div>
---
+++ ActividadPHP12/product/index.php
@@ -0,0 +1,178 @@
+<?php
+include '../app/ProductosController.php';
+include '../app/BrandController.php';
+$producto = new ProductosController;
+$productos = $producto->listaProductos();
+$brands = new BrandController;
+$brands = $brands->getBrands();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- HEAD -->
+    <?php include "../layouts/head.template.php" ?>
+</head>
+<body>
+    <!-- NAVBAR -->
+    <?php include "../layouts/nav.template.php" ?>
+    <!-- CONTAINER -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- SIDEBAR -->
+            <?php include "../layouts/sidebar.template.php" ?>
+            <!-- CONTENT -->
+            <div class="col-lg-10 col-sm-12 bg-white">
+                <!-- BREAD -->
+                <div class="border-bottom">
+                    <div class="row m-2">
+                        <div class="col">
+                            <h4>Productos</h4>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-info float-end" data-bs-toggle="modal" data-bs-target="#añadirModal">Añadir producto</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- CARD -->
+                <div class="row">
+                    <?php foreach ($productos as $lista) : ?>
+                        <div class="col-md-3 p-2">
+                            <div class="card mb-1" style="width: 18rem;">
+                                <img src="<?php echo $lista->cover ?>" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center"><?php echo $lista->name ?></h5>
+                                    <h6 class="card-title text-center"><?php echo ($lista->brand == null ? "No selecciono categoria" : $lista->brand->name) ?></h6>
+                                    <p class="card-text"><?php echo $lista->description ?></p>
+                                    <div class="row">
+                                        <a data-product='<?php echo json_encode($lista)?>' onclick="llenarDatos(this)" class="btn btn-warning col-6" data-bs-toggle="modal" data-bs-target="#updateProduct">Editar</a>
+                                        <a class="btn btn-danger  col-6"onclick="remove(<?php echo $lista->id ?>)">Eliminar</a>
+                                        <a href="<?= BASE_PATH."prod/".$lista->slug ?>" class="btn btn-info col-12">Detalles</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--FUNCION AÑADIR-->
+<div class="modal fade" id="añadirModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="añadirModalLabel"> ->Nuevo Producto<- </h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form method="post" action="<?= BASE_PATH?>prod" enctype="multipart/form-data">
+                    <div class="modal-body">
+                        <input type="file" name="img_producto">
+                        <input type="text" name="name" class="form-control" placeholder="name" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="text" name="slug" class="form-control" placeholder="slug" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="text" name="description" class="form-control" placeholder="description" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="text" name="features" class="form-control" placeholder="features" aria-label="Username" aria-describedby="basic-addon1">
+                        <select name="brand_id" class="form-control">
+                                <?php foreach ($brands as $brand) 
+                                { ?>
+                                    <option value="<?php echo $brand->id; ?>"><?php echo $brand->name; ?></option>
+                                <?php 
+                            }?>
+                            </select>
+                    </div>
+                    <input type="hidden" name="action" value="create">
+                    <input type="hidden" name="super_token" value="<?=$_SESSION['super_token'] ?>">
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">cerrar</button>
+                        <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Modal: "EDITAR" -->
+    <div class="modal fade" id="updateProduct" tabindex="-1" aria-labelledby="updateProduct" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modificar Articulo</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="../app/ProductosController.php" method="POST" enctype="multipart/form-data">
+                <div class="modal-body">
+                        <span class="input-group-text" id="addon-wrapping">Nombre</span>
+                        <input type="text" id="name"  name="name" class="form-control" placeholder="">
+                        <span class="input-group-text" id="addon-wrapping">Slug</span>
+                        <input type="text" id="slug" name="slug" class="form-control" placeholder="slug" aria-label="Username" aria-describedby="basic-addon1">
+                        <span class="input-group-text" id="addon-wrapping">Descripcion</span>
+                        <input type="text" id="description" name="description" class="form-control" placeholder="">
+                        <span class="input-group-text" id="addon-wrapping">Caracteristicas</span>
+                        <input type="text" id="features" name="features" class="form-control" placeholder="">
+                        <span class="input-group-text" id="addon-wrapping">Brand</span>
+                        <select class="form-select" aria-label="Default select example" id="brand_id" name="brand_id">
+                            <option selected>Selecciona la marca</option>
+                            <?php foreach($brands as $brand): ?>
+                                <option value="<?php echo $brand->id; ?>"><?php echo $brand->name; ?></option>
+                            <?php endforeach; ?>
+                          </select>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <input id="inputOculto" type="hidden" name="action" value="">
+                    <input type="hidden" name="super_token" value="<?=$_SESSION['super_token'] ?>">
+                    <input type="text" name="id" id="id" value="0">
+                    <button type="submit" class="btn btn-primary">Editar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+    <?php include "../layouts/scripts.template.php" ?>
+    <!-- FUNCION: "ELIMINAR" -->
+    <!-- FUNCION: "EDITAR" -->
+    <script>
+        function remove (id) {
+        swal({
+        title: "Estas seguro??",
+        text: "Una vez eliminado no podras recuperar el archivo",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+        
+        })
+        .then((willDelete) => {
+         
+        if (willDelete) {
+            swal("Producto Eliminado Con exito", {
+            icon: "success",
+            });
+            var bodyFormData = new FormData();
+            bodyFormData.append('id', id);
+            bodyFormData.append('action', 'delete');
+            bodyFormData.append('super_token', '<?=$_SESSION['super_token'] ?>');
+            axios.post('<?= BASE_PATH?>prod', bodyFormData)
+            .then(function (response) {
+                console.log(response);
+                location.reload();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        } else {
+            swal("Cancelado con exito");
+        }
+        });
+    }
+function llenarDatos(target) {
+            let product = JSON.parse(target.getAttribute('data-product'));
+               document.getElementById("inputOculto").value = "upload";
+            console.log(product);
+            document.getElementById("name").value = product.name;
+            document.getElementById("slug").value = product.slug;
+            document.getElementById("description").value = product.description;
+            document.getElementById("features").value = product.features;
+            document.getElementById("brand_id").value = product.brand_id;
+            document.getElementById("id").value = product.id;
+};
+    </script>
+</body>
+</html>
